7fdd43559bf86d2e99fe7cc7c32ca22e76544f0f
Jahn Alexander Hane <jahn.hane@plentymarkets.com>
2017-02-15 09:01:14 +0100

    Mops list, andere anzeige

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -38,7 +38,7 @@ class SettingsController extends Controller
             /**
              * @var $mopModel Plenty\Modules\Payment\Method\Models\PaymentMethod
              */
-            $response['methodsOfPayment'][$mopModel->paymentKey] = $mopModel->id;
+            $response['methodsOfPayment'][] = $mopModel->toArray();
         }
 
         return $response;
